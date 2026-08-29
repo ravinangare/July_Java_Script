dbc6af6220b758cdf638d8931f8ec5c0711c8491
ravinangare <ravinangre@gmail.com>
2026-08-29 08:37:27 +0530

    css selector locator

--- tests/css.spec.js
+++ tests/css.spec.js
@@ -25,3 +25,51 @@ test('css Selector test',async({page})=>{
     //await page.locator("input.mr-sm-2.form-control#lastName").fill("Bokade")
     await page.locator('input.mr-sm-2.form-control[placeholder="Last Name"]').fill("Bokade")
 })
+
+test('css with tagname',async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    await expect(page.locator('div[class="orangehrm-login-footer-sm"]>a:last-child')).toBeVisible()
+    await expect(page.locator('div[class="orangehrm-login-footer-sm"]>a:nth-child(2)')).toBeVisible()
+    await expect(page.locator('div[class="orangehrm-login-footer-sm"]>a:nth-child(3)')).toBeVisible()
+    await expect(page.locator('div[class="orangehrm-login-footer-sm"]>a:first-child')).toBeVisible()
+    await page.locator('[name="username"]').fill("admin")
+    await page.locator('[name="password"]').fill("admin123")
+    await page.locator('button').click()
+})
+
+test('css with direct child',async({page})=>{
+        await page.goto("https://demoqa.com/automation-practice-form")
+        await page.locator("div[class='form-check form-check-inline']>input[value='Male']").click();
+        await page.locator("div>button").click()
+})
+
+test('css with descendent',async({page})=>{
+        await page.goto("https://demoqa.com/automation-practice-form")
+        await page.locator("div button").last().click()
+})
+
+
+test('css with Adjacent Sibling',async({page})=>{
+        await page.goto("https://demoqa.com/automation-practice-form")
+        await page.locator("input[value='Female'] + label").click()
+})
+
+test('css with first & last child',async({page})=>{
+    await page.goto("https://demoqa.com/automation-practice-form")
+    await page.locator("div[class='form-check form-check-inline']:first-child").first().click()
+    await page.locator("div[class='form-check form-check-inline']:first-child").last().click()
+
+    await page.locator("div[class='form-check form-check-inline']:last-child").first().click()
+    await page.locator("div[class='form-check form-check-inline']:last-child").last().click()
+
+    await page.locator("div[class='form-check form-check-inline']:nth-child(2)").first().click()
+    await page.locator("div[class='form-check form-check-inline']:nth-child(2)").last().click()
+})
+
+test('starts with, Ends With & contains',async({page})=>{
+      await page.goto("https://demoqa.com/automation-practice-form")
+    await page.locator('[placeholder^="Mobile"]').fill('9423111564')    // strats with
+    await page.locator('[placeholder$="Address"]').fill('Ambegaon Pune') // ends with
+    await page.locator('[id*="jectsInp"]').fill('English')  // contains
+})
