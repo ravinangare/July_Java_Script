d34dafa65aeb9d4932e2dff4d9be7fb0c46834a9
ravinangare <ravinangre@gmail.com>
2026-09-01 08:30:03 +0530

    xpath Axes methods

--- tests/xpath.spec.js
+++ tests/xpath.spec.js
@@ -32,3 +32,28 @@ test('Xpath Test using multiple Attributes',async({page})=>{
     await page.locator('//input[@class=" mr-sm-2 form-control" and @placeholder="First Name"]').fill("Riddhi")
     await page.locator('//input[@class="-sm-2 form-control" or @placeholder="Last Name"]').fill('Ganorkar')
 })
+test('Xpath Axes methods',async({page})=>{
+    await page.goto("https://www.amazon.in")
+    await page.getByPlaceholder('Search Amazon.in').fill('Andriod Phone')
+    await page.locator('#nav-search-submit-button').click()
+    // Xpath Axes methods parent Child
+    // Syntax
+    // //tagname[@attribute='value']//child::tagname
+    // parent
+    // child
+    // sibling
+    // ancestors
+    // descendents
+    // following
+    // following-sibling
+    // self
+
+   // await page.locator("//span[text()='Get It Today']//parent::a").click()
+   //  await page.locator("//span[text()='Get It Today']//preceding-sibling::div").click()
+   // await page.locator("//span[text()='Get It Today']//ancestor::span[@role='presentation' and @data-csa-c-type='element']").click()
+ //   await page.locator('//span[@data-component-type="s-search-results"]//child::div[@class="a-section"]//child::h2//child::span').first().click()
+//    await page.locator('//span[@data-component-type="s-search-results"]//div[@class="a-section"]//h2/child:://span').nth(2).click()
+    //span[@data-component-type="s-search-results"]//div[@class="a-section"]
+   // await page.locator("//a[text()='Amazon Global Selling']//parent::li//following-sibling::li").first().click()
+    await page.locator("//a[text()='Amazon Global Selling']//parent::li//preceding-sibling::li").first().click()
+})
